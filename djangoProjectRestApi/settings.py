1931fce3ae2a24b1b02e78034aca6888c417e1e5
Feroz Khan <muhammad.feroz@devsinc.com>
2021-12-16 19:15:45 +0500

    initial

--- djangoProjectRestApi/settings.py
+++ djangoProjectRestApi/settings.py
@@ -28,6 +28,7 @@ SECRET_KEY = 'django-insecure-@5lr!q!*p&e6&tj+b*2_kmoxho-ske9*mg^bg-ajpqff@-an0l
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'project-rest-api-test.herokuapp.com/']
 
@@ -150,6 +151,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
